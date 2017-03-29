35ae35fb030bf3827afd088029231aeb0736c7ba
Kévin Pausé <tzoreol@gmail.com>
2017-03-29 21:55:47 +0200

    Adding switch and led

---
+++ 3_switch_led/switch_led.ino
@@ -0,0 +1,30 @@
+const byte LED = 2;
+const byte SWITCH = 3;
+
+boolean prevSwitchState = HIGH;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(SWITCH, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Prev: ");
+  Serial.print(prevSwitchState);
+  Serial.print(" Now: ");
+  Serial.print(digitalRead(SWITCH));
+  Serial.print('\n');
+  
+  if(prevSwitchState == LOW && digitalRead(SWITCH) == HIGH) {
+    if(digitalRead(LED) == LOW) {
+      digitalWrite(LED, HIGH);
+    } else {
+      digitalWrite(LED, LOW);
+    }
+  }
+
+  prevSwitchState = digitalRead(SWITCH);
+}
